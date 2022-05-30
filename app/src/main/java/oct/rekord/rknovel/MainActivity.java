45af7d1353296f27646f221e9a00e2125006bf81
Rekord <1324596506@qq.com>
2022-05-30 11:25:36 +0800

    feat: init commit

---
+++ app/src/main/java/oct/rekord/rknovel/MainActivity.java
@@ -0,0 +1,14 @@
+package oct.rekord.rknovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
